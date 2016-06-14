a9646bc500d9e794454601cf3d254d8d51de2ff4
萩野一平 <haginoippei@haginoippei-no-MacBook-Air.local>
2016-06-14 19:54:41 +0900

    first commit

---
+++ area_volume.rb
@@ -0,0 +1,15 @@
+=begin
+「楽しいRuby 第3版」サンプル
+=end
+
+x= 10 # 横
+y = 20 # 縦
+z = 30 # 高さ
+
+# 表面積と(ry
+area = (x*y + y*z + z*x) * 2
+volume = x*y*z
+
+#出力する
+print "表面積=",area,"\n"
+print "体積=",volume,"\n"
