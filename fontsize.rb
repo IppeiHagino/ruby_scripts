a4dd1fff5714bba33069d56024907bdf357c38de
Ippei Hagino <ippei.hagino@pixta.co.jp>
2016-07-06 12:18:05 +0900

    0706 fontsize.rb の修正( " {とnormalの間に半角スペースを入れましょう。 "と}の間に半角スペースを入れましょう。")

--- fontsize.rb
+++ fontsize.rb
@@ -1,7 +1,6 @@
-print "<html><title>font size list</title>"
-puts "<body>\n<p>"
-font_table = {normal: "+0", small: "-1", big: "+1"}
+puts "<html><title>font size list</title><body>\n<p>"
+font_table = { normal: "+0", small: "-1", big: "+1" }
 font_table.each do |key, value|
- puts ("<font size =#{value}>#{key}</font><br>")
+ puts ( "<font size =#{value}>#{key}</font><br>" )
 end
 puts "</p></body></html>" 
